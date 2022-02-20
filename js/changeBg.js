a4ae2943ab158ae5a839b16ecaeab689fe89b727
seraphinaciel <99410338+seraphinaciel@users.noreply.github.com>
2022-02-20 21:06:11 +0900

    Add files via upload

--- js/changeBg.js
+++ js/changeBg.js
@@ -1,5 +1,5 @@
 const images = [
-  "01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg", "06.jpg", "07.jpg"
+  "01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg", "06.jpg", "07.jpg", "08.jpg"
 ];
 
 const visual = document.querySelector(".visual");
